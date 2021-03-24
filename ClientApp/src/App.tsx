13970b6d48dc68279f5214d80c71c84ada403ee6
kotita49 <kotita49@gmail.com>
2021-03-24 22:37:17 +0000

    initial commit

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -4,8 +4,10 @@ import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
+import 'bootstrap';
 
 import './custom.css'
+import { Conservation } from './Pages/Conservation/Conservation';
 
 export default class App extends Component {
   static displayName = App.name;
@@ -15,7 +17,7 @@ export default class App extends Component {
       <Layout>
         <Route exact path='/' component={Home} />
         <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
+        <Route path='/conservation' component={Conservation} />
       </Layout>
     );
   }
---
+++ ClientApp/src/Pages/Conservation/Conservation.tsx
@@ -0,0 +1,59 @@
+import './Conservation.scss';
+import 'bootstrap';
+import React from 'react';
+
+export function Conservation(): JSX.Element {
+    return(
+        <div>
+        <div className="container-fluid">
+            <h2 >Conservation</h2>
+        </div>
+<div className="container">
+  <div className="row">
+    <div className="col-sm">
+        col
+        <div className="card">
+  <img src="https://i.picsum.photos/id/1084/4579/3271.jpg?hmac=YblMazviSugJVfZsFPaFI_Vp6lBeQin62qpm8rxHruo" className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" className="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+    </div>
+        <div className="col-sm">col
+    <div className="card">
+  <img src="https://i.picsum.photos/id/1084/4579/3271.jpg?hmac=YblMazviSugJVfZsFPaFI_Vp6lBeQin62qpm8rxHruo" className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" className="btn btn-primary">Go somewhere</a>
+  </div>
+    </div>
+    </div>
+    <div className="col-sm">col
+    <div className="card">
+  <img src="https://i.picsum.photos/id/1084/4579/3271.jpg?hmac=YblMazviSugJVfZsFPaFI_Vp6lBeQin62qpm8rxHruo" className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" className="btn btn-primary">Go somewhere</a>
+  </div>
+    </div>
+    </div>
+    <div className="col-sm">col
+    <div className="card">
+  <img src="https://i.picsum.photos/id/1084/4579/3271.jpg?hmac=YblMazviSugJVfZsFPaFI_Vp6lBeQin62qpm8rxHruo" className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" className="btn btn-primary">Go somewhere</a>
+  </div>
+    </div>
+  </div>
+  </div>
+  </div>
+  </div>
+    );
+}
+
