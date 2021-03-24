d03a277e1c1541d4d478c69fcf9e7e6a9af31f7b
kotita49 <kotita49@gmail.com>
2021-03-24 15:00:22 +0000

    Submit sighting form

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -5,7 +5,9 @@ import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
 
+
 import './custom.css'
+import { SubmitSightingForm } from './Pages/SubmitSighting/SubmitSighting';
 
 export default class App extends Component {
   static displayName = App.name;
@@ -16,6 +18,7 @@ export default class App extends Component {
         <Route exact path='/' component={Home} />
         <Route path='/counter' component={Counter} />
         <Route path='/fetch-data' component={FetchData} />
+        <Route path='/submit-sighting' component={SubmitSightingForm} />
       </Layout>
     );
   }
