bbcbb31ada32102738fb04eec3a2a0178899c959
RoshniCh <roshni.chandrasekar@softwire.com>
2021-03-25 12:21:30 +0000

    form for admin login created

--- ClientApp/src/Pages/AdminLogin/AdminLogin.tsx
+++ ClientApp/src/Pages/AdminLogin/AdminLogin.tsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
 
-function AdminLogin() {
+function AdminLogin() : JSX.Element {
+
+    const [username, setUsername] = useState<string | null>(null);
+    const [password, setPassword] = useState<string | null>(null);
+    const submitForm = (e : FormEvent) => {
+        e.preventDefault();
+        // Code to find user in database, see if password matches
+        // If yes, log in and direct to Review Sightings page
+        // If not, display error message
+    }
+
     return (
         <div>
-
             <h1>Admin Login</h1>
-            
+            <form>
+                <label>
+                    Username:
+                    <input type="text" name="username" onChange={e => setUsername(e.target.value)} required />
+                </label>
+                <label>
+                    Password:
+                    <input type="password" name="password" onChange={e => setPassword(e.target.value)} required />
+                </label>
+                <button onClick = {submitForm}>Login</button>
+            </form>            
         </div>
     )
 }
 
-export { AdminLogin }
+export { AdminLogin };
