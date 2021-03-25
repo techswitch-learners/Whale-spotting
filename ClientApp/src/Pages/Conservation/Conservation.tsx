05c28472933126bdfc0519efb44134b85d9373cc
Callum Griffiths <Callum.Griffiths@softwire.com>
2021-03-25 17:06:41 +0000

    Conservation.tsx formatted

--- ClientApp/src/Pages/Conservation/Conservation.tsx
+++ ClientApp/src/Pages/Conservation/Conservation.tsx
@@ -6,90 +6,77 @@ import * as ConservationData from "./ConservationData.json";
 export function Conservation(): JSX.Element {
   return (
     <div>
+
       <div className="container-fluid">
-        <h2 className="conservationTitle">Conservation</h2>
-        <p className="text-justify">
-          Some good work has been done in recent decades to reverse the trend of
-          almost inevitable extinction for some species of cetaceans.
-          Unfortunately, whales and dolphins continue to be threatened by:
-        </p>
+          <h2 className="conservationTitle">Conservation</h2>
+          <p className="text-justify">
+            Some good work has been done in recent decades to reverse the trend of
+            almost inevitable extinction for some species of cetaceans.
+            Unfortunately, whales and dolphins continue to be threatened by:
+          </p>
       </div>
+
       <div className="container">
+
         <div className="row">
+
           <div className="col-sm">
             <div className="card">
               <img src={ConservationData.Pollution.imgSrc} />
               <div className="card-body">
                 <h5 className="card-title">Pollution</h5>
                 <p className="card-text">{ConservationData.Pollution.text}</p>
-                <a
-                  href={ConservationData.Pollution.siteLink}
-                  className="btn btn-primary"
-                >
+                <a href={ConservationData.Pollution.siteLink} className="btn btn-primary">
                   Read more
                 </a>
               </div>
             </div>
           </div>
+
           <div className="col-sm">
             <div className="card">
-              <img
-                src={ConservationData.Nets.imgSrc}
-                className="card-img-top"
-                alt="..."
-              />
+              <img src={ConservationData.Nets.imgSrc} className="card-img-top" alt="..." />
               <div className="card-body">
                 <h5 className="card-title">Nets</h5>
                 <p className="card-text">{ConservationData.Nets.text}</p>
-                <a
-                  href={ConservationData.Nets.siteLink}
-                  className="btn btn-primary"
-                >
+                <a href={ConservationData.Nets.siteLink} className="btn btn-primary">
                   Read more
                 </a>
               </div>
             </div>
           </div>
+
           <div className="col-sm">
             <div className="card">
-              <img
-                src={ConservationData.Whaling.imgSrc}
-                className="card-img-top"
-                alt="..."
-              />
+              <img src={ConservationData.Whaling.imgSrc} className="card-img-top" alt="..." />
               <div className="card-body">
                 <h5 className="card-title">Whaling</h5>
                 <p className="card-text">{ConservationData.Whaling.text}</p>
-                <a
-                  href={ConservationData.Whaling.siteLink}
-                  className="btn btn-primary"
-                >
+                <a href={ConservationData.Whaling.siteLink} className="btn btn-primary" >
                   Read more
                 </a>
               </div>
             </div>
           </div>
+
           <div className="col-sm">
             <div className="card">
               <img
-                src={ConservationData.Captivity.imgSrc}
-                className="card-img-top"
-                alt="..."
-              />
+                src={ConservationData.Captivity.imgSrc} className="card-img-top" alt="..."/>
               <div className="card-body">
                 <h5 className="card-title">Captivity</h5>
                 <p className="card-text">{ConservationData.Captivity.text}</p>
-                <a
-                  href={ConservationData.Captivity.siteLink}
-                  className="btn btn-primary"
-                >
+                <a href={ConservationData.Captivity.siteLink} className="btn btn-primary">
                   Read more
                 </a>
               </div>
             </div>
           </div>
+
         </div>
+
       </div>
+
       <p className="border border-dark">
         You can help immediately by donating to marine-life charities or by
         adopting a whale! To make a donation to SEA LIFE Trust, please click{" "}
@@ -101,6 +88,7 @@ export function Conservation(): JSX.Element {
         </a>
         .
       </p>
+      
     </div>
   );
 }
