812c06cabefaafaa1c6b68b172ee700abef75326
kotita49 <kotita49@gmail.com>
2021-03-25 10:18:36 +0000

    init commit

--- ClientApp/src/Pages/Conservation/Conservation.tsx
+++ ClientApp/src/Pages/Conservation/Conservation.tsx
@@ -13,7 +13,7 @@ export function Conservation(): JSX.Element {
     <div className="col-sm">
         col
         <div className="card">
-  <img src="https://i.picsum.photos/id/1084/4579/3271.jpg?hmac=YblMazviSugJVfZsFPaFI_Vp6lBeQin62qpm8rxHruo" className="card-img-top" alt="..."/>
+  <img src="https://images.unsplash.com/photo-1568430462989-44163eb1752f?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=3008&q=80" className="card-img-top" alt="..."/>
   <div className="card-body">
     <h5 className="card-title">Card title</h5>
     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
