7039f61850a7ee6c06e82ec51505ff80ca66095f
kotita49 <kotita49@gmail.com>
2021-03-25 14:28:13 +0000

    Conservation page with tests and css

--- ClientApp/src/Pages/Conservation/Conservation.tsx
+++ ClientApp/src/Pages/Conservation/Conservation.tsx
@@ -1,6 +1,9 @@
 import './Conservation.scss';
 import 'bootstrap';
 import React from 'react';
+import * as ConservationData from './ConservationData.json';
+
+
 
 export function Conservation(): JSX.Element {
     return(
@@ -15,47 +18,49 @@ Unfortunately, whales and dolphins continue to be threatened by:</p>
     <div className="col-sm">
         
         <div className="card">
-  <img src="https://images.unsplash.com/photo-1558640476-437a2b9438a2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&h=800" className="card-img-top" alt="..."/>
+  <img src={ConservationData.Pollution.imgSrc}/>
   <div className="card-body">
     <h5 className="card-title">Pollution</h5>
-    <p className="card-text">Studies of whale and dolphin tissues from around the world show significant levels of persistent organic pollutants (POPs) and endocrine disrupting chemicals (EDCs). In some cases, levels of these chemicals are high enough to cause damage to both reproductive and immune systems.
+    <p className="card-text">{ConservationData.Pollution.text}
 </p>
-    <a href="https://wwf.panda.org/discover/knowledge_hub/endangered_species/cetaceans/threats/pollution/" className="btn btn-primary">Read more</a>
+    <a href={ConservationData.Pollution.siteLink} className="btn btn-primary">Read more</a>
   </div>
 </div>
     </div>
         <div className="col-sm">
     <div className="card">
-  <img src="https://images.unsplash.com/photo-1525381098317-fc5822b4c483?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&h=800" className="card-img-top" alt="..."/>
+  <img src={ConservationData.Nets.imgSrc} className="card-img-top" alt="..."/>
   <div className="card-body">
     <h5 className="card-title">Nets</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" className="btn btn-primary">Read more</a>
+    <p className="card-text">{ConservationData.Nets.text}</p>
+    <a href={ConservationData.Nets.siteLink} className="btn btn-primary">Read more</a>
   </div>
     </div>
     </div>
     <div className="col-sm">
     <div className="card">
-  <img src="https://images.unsplash.com/photo-1602025035269-40c624bfc2c9?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&h=800" className="card-img-top" alt="..."/>
+  <img src={ConservationData.Whaling.imgSrc} className="card-img-top" alt="..."/>
   <div className="card-body">
     <h5 className="card-title">Whaling</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" className="btn btn-primary">Read more</a>
+    <p className="card-text">{ConservationData.Whaling.text}</p>
+    <a href={ConservationData.Whaling.siteLink} className="btn btn-primary">Read more</a>
   </div>
     </div>
     </div>
     <div className="col-sm">
     <div className="card">
-  <img src="https://images.unsplash.com/photo-1522432214543-7ba98bbfa10f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&h=800" className="card-img-top" alt="..."/>
+  <img src={ConservationData.Captivity.imgSrc} className="card-img-top" alt="..."/>
   <div className="card-body">
     <h5 className="card-title">Captivity</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" className="btn btn-primary">Read more</a>
+    <p className="card-text">{ConservationData.Captivity.text}</p>
+    <a href={ConservationData.Captivity.siteLink} className="btn btn-primary">Read more</a>
   </div>
     </div>
   </div>
   </div>
   </div>
+  <p className="border border-dark">You can help immediately by donating to marine-life charities or by adopting a whale! To make a donation to SEA LIFE Trust, please click <a href="https://www.sealifetrust.org/en/"> here</a>. To adopt a marine animal, click <a href="https://www.pacificwhale.org/you-can-help/adopt-marine-animal/"> here</a>.
+</p>
   </div>
     );
 }
