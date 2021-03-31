08d6cf1bdab9f6482f10e38a701ae3887f446092
Yvonne Wong <Yvonne.Wong@softwire.com>
2021-03-31 10:44:22 +0100

    commit before switching branches

--- Models/Database/AdminUser.cs
+++ Models/Database/AdminUser.cs
@@ -8,12 +8,12 @@ namespace whale_spotting.Models.Database
     //IdentityUser is found in the metadata of the login app :S//
     public class AdminUser : IdentityUser
     {
-        // [Key]
-        // [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        // public int Id { get; set; }
-        // public string Username { get; set; }
-        // public string HashedPassword { get; set; }
-        // public string Salt { get; set; }
-        // public string  Email { get; set; }
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string HashedPassword { get; set; }
+        public string Salt { get; set; }
+        public string  Email { get; set; }
     }
 }
