dc2168e2ea8ec62e7d8b8b54b63d1b15eb3c3843
ZOO\MarAza <Mariam.Azarian@softwire.com>
2021-04-06 14:36:16 +0100

    test fixed

--- ClientApp/src/App.test.tsx
+++ ClientApp/src/App.test.tsx
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
+jest.mock("./Api/apiClient", () => {
+  return {
+    getRecentSightings: jest.fn().mockImplementation(() => {
+      return Promise.resolve(null)
+    }),
+  };
+});
 
 it('renders without crashing', async () => {
   const div = document.createElement('div');
--- ClientApp/src/Pages/Home/Home.test.tsx
+++ ClientApp/src/Pages/Home/Home.test.tsx
@@ -6,8 +6,90 @@ import { fireEvent, render, screen,act } from '@testing-library/react';
 
 
 
-test('renders the Home component without errors', () => {
-  render(<Home />)
+
+
+jest.mock("../../Api/apiClient", () => {
+  return {
+    getRecentSightings: jest.fn().mockImplementation(() => {
+      return Promise.resolve({recentSightingsList :[
+        {
+        "id": 107297,
+        "apiId": null,
+        "species": "Orca",
+        "quantity": "18",
+        "location": "Scotland",
+        "latitude": 123,
+        "longitude": -11,
+        "description": "spotted 20 orca",
+        "sightedAt": "2021-03-31T00:00:00",
+        "submittedByName": "Roshni",
+        "submittedByEmail": "r@r.com"
+      },
+      {
+        "id": 107297,
+        "apiId": null,
+        "species": "Orce",
+        "quantity": "18",
+        "location": "Scotland",
+        "latitude": 123,
+        "longitude": -11,
+        "description": "spotted 20 orca",
+        "sightedAt": "2021-03-31T00:00:00",
+        "submittedByName": "Roshni",
+        "submittedByEmail": "r@r.com"
+      },
+      {
+        "id": 107297,
+        "apiId": null,
+        "species": "Orce",
+        "quantity": "18",
+        "location": "Scotland",
+        "latitude": 123,
+        "longitude": -11,
+        "description": "spotted 20 orca",
+        "sightedAt": "2021-03-31T00:00:00",
+        "submittedByName": "Roshni",
+        "submittedByEmail": "r@r.com"
+      },
+      {
+        "id": 107297,
+        "apiId": null,
+        "species": "Orce",
+        "quantity": "18",
+        "location": "Scotland",
+        "latitude": 123,
+        "longitude": -11,
+        "description": "spotted 20 orca",
+        "sightedAt": "2021-03-31T00:00:00",
+        "submittedByName": "Roshni",
+        "submittedByEmail": "r@r.com"
+      },
+      {
+        "id": 107297,
+        "apiId": null,
+        "species": "Orce",
+        "quantity": "18",
+        "location": "Scotland",
+        "latitude": 123,
+        "longitude": -11,
+        "description": "spotted 20 orca",
+        "sightedAt": "2021-03-31T00:00:00",
+        "submittedByName": "Roshni",
+        "submittedByEmail": "r@r.com"
+      }
+
+     ]} );
+    }),
+  };
+});
+
+it('renders homeform successfully', async () => {
+  const div = document.createElement('div');
+  ReactDOM.render(
+    <MemoryRouter>
+      <Home />
+    </MemoryRouter>, div);
+  await new Promise(resolve => setTimeout(resolve, 1000));
 });
 
 
