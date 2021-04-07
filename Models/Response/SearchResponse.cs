93ad05285716063de57a9b0318a971ee838962f3
kotita49 <kotita49@gmail.com>
2021-04-07 10:28:49 +0100

    work on progress?

--- Models/Response/SearchResponse.cs
+++ Models/Response/SearchResponse.cs
@@ -5,22 +5,36 @@ using whale_spotting.Request;
 
 namespace whale_spotting.Models.Response
 {
-    public class ListResponse<T>
+    public class ListResponse
     {
         private readonly string _path;
+
         private readonly string _filters;
+
         public IEnumerable<SightingResponse> Sightings { get; }
+
         public int TotalNumberOfItems { get; }
+
         public int Page { get; }
+
         public int PageSize { get; }
 
-        public string NextPage => !HasNextPage() ? null : $"/{_path}?page={Page + 1}&pageNumber={PageSize}{_filters}";
+        public string NextPage =>
+            !HasNextPage()
+                ? null
+                : $"/{_path}?page={Page + 1}&pageNumber={PageSize}{_filters}";
 
-        public string PreviousPage => Page <= 1 ? null : $"/{_path}?page={Page - 1}&pageNumber={PageSize}{_filters}";
+        public string PreviousPage =>
+            Page <= 1
+                ? null
+                : $"/{_path}?page={Page - 1}&pageNumber={PageSize}{_filters}";
 
-
-
-        public ListResponse(SearchRequest search, IEnumerable<SightingResponse> sightings, int totalNumberOfItems, string path)
+        public ListResponse(
+            SearchRequest search,
+            IEnumerable<SightingResponse> sightings,
+            int totalNumberOfItems,
+            string path
+        )
         {
             Sightings = sightings;
             TotalNumberOfItems = totalNumberOfItems;
@@ -29,20 +43,36 @@ namespace whale_spotting.Models.Response
             _path = path;
             _filters = search.Filters;
         }
+
         private bool HasNextPage()
         {
             return Page * PageSize < TotalNumberOfItems;
         }
     }
-    public class SearchResponse : ListResponse<SightingResponse>
+
+    public class SearchResponse : ListResponse
     {
-        private SearchResponse(SearchRequest search, IEnumerable<SightingResponse> sightings, int totalNumberOfItems) 
-            : base(search, sightings, totalNumberOfItems, "sightings") { }
-        
-    public static SearchResponse Create(SearchRequest search, IEnumerable<Sighting> sightings, int totalNumberOfItems)
+        private SearchResponse(
+            SearchRequest search,
+            IEnumerable<SightingResponse> sightings,
+            int totalNumberOfItems
+        ) :
+            base(search, sightings, totalNumberOfItems, "sightings")
         {
-            var sightingModels = sightings.Select(sighting => new SightingResponse(sighting));
-            return new SearchResponse(search, sightingModels, totalNumberOfItems);
+        }
+
+        public static SearchResponse
+        Create(
+            SearchRequest search,
+            IEnumerable<Sighting> sightings,
+            int totalNumberOfItems
+        )
+        {
+            var sightingModels =
+                sightings.Select(sighting => new SightingResponse(sighting));
+            return new SearchResponse(search,
+                sightingModels,
+                totalNumberOfItems);
         }
     }
 }
