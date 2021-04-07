22f96b0296c03e5a8ec652b7541e0d46b8e2789c
ZOO\MarAza <Mariam.Azarian@softwire.com>
2021-04-07 13:31:14 +0100

    test problem

--- ClientApp/src/Pages/WhaleSpecies/WhaleSpecies.tsx
+++ ClientApp/src/Pages/WhaleSpecies/WhaleSpecies.tsx
@@ -1,6 +1,12 @@
 import "./WhaleSpecies.scss";
 import "bootstrap";
 import React from "react";
-import {WhaleData} from "./WhaleSpeciesDataData";
+import {WhaleData} from "./WhaleSpeciesData";
 
+export function WhaleSpecies(): JSX.Element {
+  
+    return (
+        <p>To be written</p>
+    );
 
+}
